0809d00651ada7f769ee528cbbcf47c9ffb2d19a
Gilson Bezerra <gilson.bezerra@live.com>
2024-01-17 14:37:16 -0300

    Adiciona novo tema e inicia a tela de detalhes

--- app/src/main/java/com/mercadolivre/melitest/presentation/view/ProductDetailScreen.kt
+++ app/src/main/java/com/mercadolivre/melitest/presentation/view/ProductDetailScreen.kt
@@ -1,11 +1,15 @@
 package com.mercadolivre.melitest.presentation.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.mercadolivre.melitest.model.Product
 import com.mercadolivre.melitest.presentation.viewmodel.ProductDetailViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ProductDetailScreen(
     viewModel: ProductDetailViewModel = koinViewModel(),
-    product: String,
-) {}
+    product: Product,
+) {
+    Text(text = product.title)
+}
